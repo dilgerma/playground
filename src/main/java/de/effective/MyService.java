0c8ddc3bc606a00dab86c5c1f0ca0e996941e133
Martin <martin@effectivetrainings.de>
2012-03-25 21:03:01 +0200

    moved to subdir

--- src/main/java/de/effective/MyService.java
+++
@@ -1,9 +0,0 @@
-package de.effective;
-
-
-import org.springframework.jmx.export.annotation.ManagedOperation;
-import org.springframework.jmx.export.annotation.ManagedResource;
-
-public interface MyService {
-    public void sagDochMal(String was);
-}
